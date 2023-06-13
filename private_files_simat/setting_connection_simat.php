2966123d98f5568cf5857a6d54f27cc40756b705
ADMIN@PC-SANTI <calvosa7@gmail.com>
2023-06-12 22:40:31 -0500

    Si

---
+++ private_files_simat/setting_connection_simat.php
@@ -0,0 +1,10 @@
+<?php
+    //VALORES DE LA DB
+    const IP = 'localhost';
+    const DATA_BASE = 'postgres';
+    const PORT = '5432';
+
+    //CREDENCIALES DE ACCESO
+    const USER_PG = 'postgres';
+    const PASSWORD_PG = 'Santi0216s';
+?>
